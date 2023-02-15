74c8942449ffbb1363e879a3f90f0c66b2c0e230
Quentin Guidée <quentin.guidee@gmail.com>
2023-02-15 15:08:02 -0500

    Create error middleware

---
+++ middleware.go
@@ -0,0 +1,17 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func ErrorMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Next()
+		err := c.Errors.Last()
+		if err != nil {
+			c.JSON(-1, gin.H{
+				"message": err.Error(),
+			})
+		}
+	}
+}
