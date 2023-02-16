34905ce174ffc63005c574fcde6b756ea1a6fb8b
Quentin Guidée <quentin.guidee@gmail.com>
2023-02-15 19:16:59 -0500

    Create Redis PubSub helper

--- go.mod
+++ go.mod
@@ -2,9 +2,14 @@ module github.com/quentinguidee/microservice-core
 
 go 1.19
 
-require github.com/gin-gonic/gin v1.8.2
+require (
+	github.com/gin-gonic/gin v1.8.2
+	github.com/redis/go-redis/v9 v9.0.2
+)
 
 require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
--- pubsub/pubsub.go
+++ pubsub/pubsub.go
@@ -1,23 +1,38 @@
 package pubsub
 
 import (
-	"bytes"
+	"context"
 	"fmt"
-	"net/http"
+	"github.com/redis/go-redis/v9"
 )
 
+var redisClient *redis.Client
+
+func InitPubSub() {
+	redisClient = redis.NewClient(&redis.Options{
+		// TODO: .env
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+}
+
+func ensureRedisInitialized() {
+	if redisClient == nil {
+		panic("Redis Client not initialized. Call InitPubSub() on initialization.")
+	}
+}
+
 func Pub(key string, message []byte) {
-	requestURL := fmt.Sprintf("http://localhost:6140/pub/%s", key)
-
-	req, err := http.NewRequest("POST", requestURL, bytes.NewBuffer(message))
+	ensureRedisInitialized()
+	err := redisClient.Publish(context.Background(), key, message).Err()
 	if err != nil {
-		fmt.Printf("%v\n", err)
+		fmt.Printf("[pubsub] Error on Pub(): %v\n", err)
 	}
-	req.Header.Set("Content-Type", "application/json")
+}
 
-	client := http.Client{}
-	_, err = client.Do(req)
-	if err != nil {
-		fmt.Printf("%v\n", err)
-	}
+func Sub(key string) *redis.PubSub {
+	ensureRedisInitialized()
+	subscriber := redisClient.Subscribe(context.Background(), key)
+	return subscriber
 }
